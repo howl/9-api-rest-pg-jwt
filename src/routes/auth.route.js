6b490f8c810e04f03927cf7c564fdf3423c6ad41
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-26 16:16:17 +0200

    implement validations to auth/new route
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
+const { body } = require("express-validator");
 
 const { createUser, loginUser, renewToken } = require("../controllers/auth.controller");
+const validateInputs = require("../middlewares/validateInputs");
 
 const router = Router();
 
-router.post("/new", createUser);
+router.post("/new", [
+  body('name', 'Name is required').not().isEmpty().trim(),
+  body('email', 'Email is required').isEmail().trim().toLowerCase(),
+  validateInputs
+], createUser);
 router.post("/", loginUser);
 router.post("/renew", renewToken);
 
