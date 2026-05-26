35e3678f2d8467636a8e3bd3a01ca66c113a8b53
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-26 16:25:09 +0200

    improve auth/new validations
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -7,8 +7,11 @@ const enforceValidations = require("../middlewares/enforceValidations");
 const router = Router();
 
 router.post("/new", [
-  body('name', 'Name is required').not().isEmpty().trim(),
-  body('email', 'Email is required').isEmail().trim().toLowerCase(),
+  body("name").trim(),
+  body("name", "Name is required").not().isEmpty(),
+  body("email").trim().toLowerCase(),
+  body("email", "Email is required").not().isEmpty(),
+  body("email", "Invalid Email").isEmail(),
   enforceValidations
 ], createUser);
 router.post("/", loginUser);
