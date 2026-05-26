4863ecafea2721299f23af7c48caf76d3a8204eb
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-26 23:35:37 +0200

    auth.controller: implement createUser
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,33 @@
-const createUser = (req, res) => {
-  res.status(405).json({
-    ok: false,
-    msg: "User creation actually uninplemented"
-  });
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user.postgres");
+
+const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    if (await User.findByEmail(email)) {
+      return res.status(409).json({
+        ok: false,
+        msg: `Email ${email} already used, try "forget my password" if you don't remember it`
+      });
+    }
+
+    const user = await User.save({ name, email, password: await bcrypt.hash(password, 12) });
+
+    res.status(201).json({
+      ok: true,
+      msg: "User registration completed",
+      data: user
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: `Server error trying to register new user email ${email}`
+    });
+  }
 };
 
 const loginUser = (req, res) => {
