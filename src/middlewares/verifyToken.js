b550dcf438c46fed3d907e779b462a6c375baaeb
David Santamaría Rogado <howl.nsp@gmail.com>
2026-05-27 12:46:45 +0200

    enblock minsToExpire with the only conditional it is used
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -8,9 +8,11 @@ const verifyToken = (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-    const minsToExpire = Math.ceil((payload.exp - Math.floor(new Date().getTime() / 1000)) / 60);
-    if (minsToExpire > 5)
-      return res.status(412).json({ message: `Token renew only happens when there is 5 minutes or less to expire, actually ${minsToExpire} minutes left` });
+    {
+      const minsToExpire = Math.ceil((payload.exp - Math.floor(new Date().getTime() / 1000)) / 60);
+      if (minsToExpire > 5)
+        return res.status(412).json({ message: `Token renew only happens when there is 5 minutes or less to expire, actually ${minsToExpire} minutes left` });
+    }
 
     req.uid = payload.uid;
     next();
